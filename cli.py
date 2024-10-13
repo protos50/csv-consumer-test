6926cfc4893bd6e1f62c202b29c0e01dcf97f632
francojzini <francojzini@gmail.com>
2024-10-13 01:10:10 -0300

    add vaccine distribution counter method

--- cli.py
+++ cli.py
@@ -11,5 +11,18 @@ def run_cli_interface():
     sex_count = covid_stats.get_sex_count()
     invalid_data = covid_stats.get_invalid_data()
 
-    print(f"Conteo de género: {sex_count}")
+    # Imprime los resultados del conteo de género
+    print("\nConteo de género: ")
+    print(f"Masculino: {sex_count.get('M', 0)}")
+    print(f"Femenino: {sex_count.get('F', 0)}")
+    
+    # Imprime la distribución de vacunas
+    print("\nDistribución de vacunas: ")
+    covid_stats.count_vaccine_distribution()
+            # Calculating the proportion of each vaccine
+    for vaccine, count in covid_stats.get_vaccine_distribution().items():
+        proportion = (count / len(covid_stats.data)) * 100
+        print(f"{vaccine}: {proportion:.2f}%")
+    print(f"Total de registros: {len(covid_stats.data)}")
+    
     print(f"Registros inválidos: {len(invalid_data)}")
--- covid_csv.py
+++ covid_csv.py
@@ -6,6 +6,7 @@ class CovidStats:
         self.data = []
         self.invalid_data = []
         self.sex_count = {}
+        self.vaccine_distribution = {}
         
     # Getters methodfor accessing the attributes
     def get_file_name(self):
@@ -20,6 +21,9 @@ class CovidStats:
     def get_sex_count(self):
         return self.sex_count
     
+    
+    def get_vaccine_distribution(self):
+        return self.vaccine_distribution
     
     def load_data(self):
         """
@@ -50,6 +54,20 @@ class CovidStats:
             else:
                 self.invalid_data.append(row)
 
+    
+    def count_vaccine_distribution(self):
+        """
+        Counts how many people have received each type of vaccine and calculates
+        the proportion of each vaccine type with respect to the total.
+        """
+        total_vaccines = 0
+
+        for row in self.data:
+            vaccine_type = row["vacuna"]
+            self.vaccine_distribution[vaccine_type] = self.vaccine_distribution.get(vaccine_type, 0) + 1
+            total_vaccines += 1
+
+            
         
     def write_invalid_data(self, output_file):
         if self.invalid_data:
