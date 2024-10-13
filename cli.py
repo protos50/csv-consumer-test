7979ee3d3222b1badca8f85c3a0247a672028569
francojzini <francojzini@gmail.com>
2024-10-13 02:04:28 -0300

    finish the main logic

--- cli.py
+++ cli.py
@@ -11,6 +11,8 @@ def run_cli_interface():
     sex_count = covid_stats.get_sex_count()
     invalid_data = covid_stats.get_invalid_data()
 
+    print(f"\nTotal de registros: {len(covid_stats.data)}")
+    
     # Imprime los resultados del conteo de género
     print("\nConteo de género: ")
     print(f"Masculino: {sex_count.get('M', 0)}")
@@ -18,11 +20,22 @@ def run_cli_interface():
     
     # Imprime la distribución de vacunas
     print("\nDistribución de vacunas: ")
-    covid_stats.count_vaccine_distribution()
-            # Calculating the proportion of each vaccine
+    covid_stats.count_vaccine_distribution() 
+    # Calculating the proportion of each vaccine
     for vaccine, count in covid_stats.get_vaccine_distribution().items():
         proportion = (count / len(covid_stats.data)) * 100
         print(f"{vaccine}: {proportion:.2f}%")
-    print(f"Total de registros: {len(covid_stats.data)}")
     
-    print(f"Registros inválidos: {len(invalid_data)}")
+    
+    # Imprimir la distribución de dosis
+    print("\nDistribución de dosis: ")
+    covid_stats.count_doses_by_jurisdiccion()
+    for jurisdiccion, count in covid_stats.get_jurisdiccion_count().items():
+        print(f"{jurisdiccion}: {count} dosis")  
+        
+    covid_stats.count_second_doses_by_jurisdiccion()
+    covid_stats.count_reforced_doses_by_age()
+    
+    # Imprime los registros inválidos
+    covid_stats.write_invalid_data("invalid_data.csv")
+    print(f"\nRegistros inválidos: {len(invalid_data)}")
--- covid_csv.py
+++ covid_csv.py
@@ -7,23 +7,30 @@ class CovidStats:
         self.invalid_data = []
         self.sex_count = {}
         self.vaccine_distribution = {}
+        self.jurisdiccion_count = {}
+        self.second_doses_jurisdiccion_count = {}
+        self.reforced_olders_count = 0
         
-    # Getters methodfor accessing the attributes
+    # Getters method for accessing the attributes
     def get_file_name(self):
         return self.file_name
     
     def get_data(self):
+        
         return self.data
     
     def get_invalid_data(self):
+        
         return self.invalid_data
 
     def get_sex_count(self):
         return self.sex_count
-    
-    
+        
     def get_vaccine_distribution(self):
         return self.vaccine_distribution
+    
+    def get_jurisdiccion_count(self):
+        return self.jurisdiccion_count
     
     def load_data(self):
         """
@@ -44,7 +51,14 @@ class CovidStats:
 
 
     def count_sexo(self):
-        valid_genders = {'F', 'M'}  # Valid genders as a set
+        """
+        Counts the number of people in each gender and stores the result in the
+        sex_count attribute. The sex_count attribute is a dictionary where the
+        keys are the genders and the values are the number of people in each gender.
+
+        Also, it stores the rows with invalid gender in the invalid_data attribute.
+        """
+        valid_genders = {'F', 'M', 'S.I.'}  # Valid genders as a set
         
         for row in self.data:
             gender = row["sexo"]
@@ -57,8 +71,10 @@ class CovidStats:
     
     def count_vaccine_distribution(self):
         """
-        Counts how many people have received each type of vaccine and calculates
-        the proportion of each vaccine type with respect to the total.
+        Counts the number of people for each vaccine type and stores the result in the
+        vaccine_distribution attribute. The vaccine_distribution attribute is a
+        dictionary where the keys are the vaccine types and the values are the number of
+        people for each vaccine type.
         """
         total_vaccines = 0
 
@@ -67,7 +83,47 @@ class CovidStats:
             self.vaccine_distribution[vaccine_type] = self.vaccine_distribution.get(vaccine_type, 0) + 1
             total_vaccines += 1
 
+    def count_doses_by_jurisdiccion(self):              
+        """
+        Counts the number of people in each jurisdiccion_residencia and stores the result in the
+        jurisdiccion_count attribute. The jurisdiccion_count attribute is a dictionary where the
+        keys are the jurisdiccion_residencia and the values are the number of people in each jurisdiccion_residencia.
+        """
+        for row in self.data:
+            jurisdiccion = row["jurisdiccion_residencia"]
+            self.jurisdiccion_count[jurisdiccion] = self.jurisdiccion_count.get(jurisdiccion, 0) + 1
+
+       
+    def count_second_doses_by_jurisdiccion(self):
+        """
+        Counts the number of people who have received the second dose in each jurisdiccion_residencia and stores the result in the
+        second_doses_jurisdiccion_count attribute. The second_doses_jurisdiccion_count attribute is a dictionary where the keys are the
+        jurisdiccion_residencia and the values are the number of people in each jurisdiccion_residencia who have received the second dose.
+        """
+        for row in self.data:
+            if row["orden_dosis"] == '2':  
+                jurisdiccion = row["jurisdiccion_residencia"]
+                self.second_doses_jurisdiccion_count[jurisdiccion] = self.second_doses_jurisdiccion_count.get(jurisdiccion, 0) + 1
+
+        # Imprimir el resultado
+        for jurisdiccion, count in self.second_doses_jurisdiccion_count.items():
+            print(f"{jurisdiccion}: {count} personas recibieron la segunda dosis")
+
+    def count_reforced_doses_by_age(self):
+        """
+        Counts the number of people that have received a refuerzo dose and are older than 60 years old.
+        The result is stored in the reforced_olders_count attribute.
+        """
+        for row in self.data:
+            grupo_etario = row["grupo_etario"]
+            doses_name = row["nombre_dosis_generica"]
             
+            is_grater_60 = (grupo_etario.startswith('60') or grupo_etario.startswith('70') or grupo_etario.startswith('80') or grupo_etario.startswith('90'))
+            if doses_name == 'Refuerzo' and is_grater_60:
+                self.reforced_olders_count += 1
+
+        print(f"Personas mayores de 60 años que han recibido dosis de refuerzo: {self.reforced_olders_count}")
+
         
     def write_invalid_data(self, output_file):
         if self.invalid_data:
